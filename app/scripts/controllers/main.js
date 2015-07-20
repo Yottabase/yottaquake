83b047f2cbb4082e20f2bf1bb32f660a2c9b1d14
Christian Vadalà <chr.vadala@gmail.com>
2015-07-20 14:32:40 +0200

    crea struttura progetto

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yottaquakeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yottaquakeApp
+ */
+angular.module('yottaquakeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
