e9df2ade2767274dc46d9131700585eb7c87d204
Christian Vadalà <chr.vadala@gmail.com>
2015-07-21 17:17:15 +0200

    code clean

--- yottaquake/WebContent/assets/js/events-by-month.js
+++ yottaquake/WebContent/assets/js/events-by-month.js
@@ -1,6 +1,4 @@
 jQuery(document).ready(function ($) {
-    // Code using $ as usual goes here.
-
 
     var margin = {top: 20, right: 20, bottom: 30, left: 40},
         width = 960 - margin.left - margin.right,
