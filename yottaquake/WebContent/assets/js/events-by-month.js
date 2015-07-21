97ab1d521277380b831e80d63acf30f9fe5628d6
Christian Vadalà <chr.vadala@gmail.com>
2015-07-21 17:33:59 +0200

    aggiunge barra filtri

--- yottaquake/WebContent/assets/js/events-by-month.js
+++ yottaquake/WebContent/assets/js/events-by-month.js
@@ -19,8 +19,7 @@ jQuery(document).ready(function ($) {
         .orient("left")
         .ticks(10);
 
-    var svg = d3.select("body").append("svg")
-        .attr("id", "events-by-month")
+    var svg = d3.select("#events-by-month").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
