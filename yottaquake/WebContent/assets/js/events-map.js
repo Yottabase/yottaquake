7756a76e8d16b6c477560b67275c3d96751c89bd
Christian Vadalà <chr.vadala@gmail.com>
2015-07-23 16:27:17 +0200

    aggiunge credits

--- yottaquake/WebContent/assets/js/events-map.js
+++ yottaquake/WebContent/assets/js/events-map.js
@@ -6,39 +6,12 @@ jQuery(document).ready(function ($) {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			'Imagery © <a href="http://mapbox.com">Mapbox</a>, ' +
+			'Dev by <strong>Yottabase</strong>',
 		id: 'mapbox.streets'
 	}).addTo(map);
 
-
-	L.marker([51.5, -0.09]).addTo(map)
-		.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-
-	L.circle([51.508, -0.11], 500, {
-		color: 'red',
-		fillColor: '#f03',
-		fillOpacity: 0.5
-	}).addTo(map).bindPopup("I am a circle.");
-
-	L.polygon([
-		[51.509, -0.08],
-		[51.503, -0.06],
-		[51.51, -0.047]
-	]).addTo(map).bindPopup("I am a polygon.");
-
-
-	var popup = L.popup();
-
-	function onMapClick(e) {
-		popup
-			.setLatLng(e.latlng)
-			.setContent("You clicked the map at " + e.latlng.toString())
-			.openOn(map);
-	}
-
-	map.on('click', onMapClick);
-
-
+	
 	$.getJSON(wsUrl + "api-countries.do?levelQuality=low", function(data){
 		
 		var data = 
