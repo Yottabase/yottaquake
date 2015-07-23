9bfa39a25b37c49131ff1dda7c3a6ab4a65db0ca
Christian Vadalà <chr.vadala@gmail.com>
2015-07-23 16:24:37 +0200

    implementa layer paesi su mappa

--- yottaquake/WebContent/assets/js/events-map.js
+++ yottaquake/WebContent/assets/js/events-map.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
    
-	var map = L.map('chart-events-map').setView([51.505, -0.09], 13);
+	var map = L.map('chart-events-map').setView([51.505, -0.09], 4);
 
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
 		maxZoom: 18,
@@ -39,6 +39,58 @@ jQuery(document).ready(function ($) {
 	map.on('click', onMapClick);
 
 
+	$.getJSON(wsUrl + "api-countries.do?levelQuality=low", function(data){
+		
+		var data = 
+			{
+				"type": "FeatureCollection",
+				"features": data 
+			};
+		
+		var geoLayer = L.geoJson(data, {
+			style: function (feature) {
+				return {
+			        fillColor: '#' + Math.random().toString(16).substring(2, 8),
+			        weight: 1,
+			        opacity: 1,
+			        color: 'white',
+			        dashArray: '3',
+			        fillOpacity: 0.7
+			    };
+			}, 
+			onEachFeature: function (feature, layer) {
+			    layer.on({
+			        click: function(e){
+			        	//map.fitBounds(e.target.getBounds());
+			        	console.log(feature.properties.name);
+			        	
+			        	geoLayer.eachLayer(function (_l) {
+			        		
+			        		if( _l._leaflet_id == layer._leaflet_id){
+			        			
+			        			//selezionato 
+			        			_l.setStyle({
+			        				weight: 3,
+			        				fillColor: '#' + Math.random().toString(16).substring(2, 8),
+			        			});
+			        		}else{
+			        			//non selezionato 
+			        			_l.setStyle({
+			        				weight: 1,
+			        				fillColor: "transparent",
+			        			});
+			        		}
+			        	    
+			        	});
+			        	
+			        }
+			    });
+			}
+		}).addTo(map);
+		
+	});
+	
+	
 });
 		
 	
