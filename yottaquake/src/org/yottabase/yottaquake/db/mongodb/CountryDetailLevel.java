b9a2dee67688a0791cd7f6ff671f785d84a5311e
Leonardo Tilomelli <leonardo.tilomelli@gmail.com>
2015-07-27 15:21:31 +0200

    Code cleaning

--- yottaquake/src/org/yottabase/yottaquake/db/mongodb/CountryDetailLevel.java
+++ yottaquake/src/org/yottabase/yottaquake/db/mongodb/CountryDetailLevel.java
@@ -5,18 +5,5 @@ public enum CountryDetailLevel {
 	LOW,
 	MEDIUM,
 	HIGH;
-	
-	public CountryDetailLevel getValueFor(String s) {
-		switch (s) {
-		case "low":
-			return LOW;
-		case "medium":
-			return MEDIUM;
-		case "high":
-			return HIGH;
-		default:
-			return null;
-		}
-	}
 
 }
