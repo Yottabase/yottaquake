494ee7d2f3fc17b0abe5a0531244f7d96cd2b621
Christian Vadalà <chr.vadala@gmail.com>
2015-07-28 17:50:43 +0200

    Implementa countries with count

--- yottaquake/src/org/yottabase/yottaquake/ui/web/action/api/CountriesAction.java
+++ yottaquake/src/org/yottabase/yottaquake/ui/web/action/api/CountriesAction.java
@@ -9,7 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.bson.Document;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.yottabase.yottaquake.core.BoundingBox;
 import org.yottabase.yottaquake.core.CountryDetailLevel;
+import org.yottabase.yottaquake.core.LatLng;
 import org.yottabase.yottaquake.db.DBFacade;
 import org.yottabase.yottaquake.db.DBAdapterManager;
 import org.yottabase.yottaquake.ui.web.core.AbstractAction;
@@ -20,18 +22,35 @@ public class CountriesAction extends AbstractAction{
 	public void run(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		// init bbox
+		BoundingBox box = null;
+		
+		String paramTopLeftLat  =  this.cleanParam(request.getParameter("topLeftLat"));
+		String paramTopLeftLng  =  this.cleanParam(request.getParameter("topLeftLng"));
+		String paramBottomRightLat  =  this.cleanParam(request.getParameter("bottomRightLat"));
+		String paramBottomRightLng  =  this.cleanParam(request.getParameter("bottomRightLng"));
+		
+		if(paramTopLeftLat != null && paramTopLeftLng != null & paramBottomRightLat != null & paramBottomRightLng != null){
+			
+			LatLng topLeft = new LatLng(Double.parseDouble(paramTopLeftLat), Double.parseDouble(paramTopLeftLng));
+			LatLng bottomRight = new LatLng(Double.parseDouble(paramBottomRightLat), Double.parseDouble(paramBottomRightLng));
+			
+			box = new BoundingBox(topLeft, bottomRight);
+		}
+		
+		//init levelQuality		
 		String mapDetailParam =  this.cleanParam(request.getParameter("levelQuality"));
-		CountryDetailLevel mapDetailLevel = CountryDetailLevel.valueOf(mapDetailParam);
-		if(mapDetailLevel == null){
+		if(mapDetailParam == null){
 			response.getWriter().write("Parametro levelQuality richiesto");
 			return;
 		}
+		CountryDetailLevel mapDetailLevel = CountryDetailLevel.valueOf(mapDetailParam);
+		
 		
 		DBFacade facade = DBAdapterManager.getFacade();
 		
 		JSONArray items = new JSONArray();
-		
-		for(Document doc : facade.getCountries(mapDetailLevel)){
+		for(Document doc : facade.getCountriesWithEventsCount(mapDetailLevel, box)){
 			
 			JSONObject obj = new JSONObject(doc.toJson());
 			items.put(obj);
@@ -40,8 +59,7 @@ public class CountriesAction extends AbstractAction{
 		
 		response.setContentType(this.CONTENT_TYPE_JSON);
 		response.getWriter().write(items.toString());
-		
-//		facade.close();
+
 	}
 
 }
