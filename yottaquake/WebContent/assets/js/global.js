06f174248e23ed7a59c6949eb669f687a36a7e4e
Christian Vadalà <chr.vadala@gmail.com>
2015-07-28 16:27:08 +0200

    date bugfix

--- yottaquake/WebContent/assets/js/global.js
+++ yottaquake/WebContent/assets/js/global.js
@@ -19,9 +19,11 @@ jQuery(document).ready(function ($) {
 		min: 0, 
 		max: 7, 
 		range: true
-	}).on('slideStop', function(e){
-		filters.minMagnitude = e.value[0];
-		filters.maxMagnitude = e.value[1];
+	}).on('change', function(e){
+		filters.minMagnitude = e.value.newValue[0];
+		filters.maxMagnitude = e.value.newValue[1];
+		console.log(filters);
+		console.log(e);
 		$(document).trigger(eventTrigger, filters);
 	});
 	
@@ -29,25 +31,27 @@ jQuery(document).ready(function ($) {
 		min: 0, 
 		max: 100, 
 		range: true
-	}).on('slideStop', function(e){
-		filters.minDepth = e.value[0];
-		filters.maxDepth = e.value[1];
+	}).on('change', function(e){
+		filters.minDepth = e.value.newValue[0];
+		filters.maxDepth = e.value.newValue[1];
 		$(document).trigger(eventTrigger, filters);
 	});
 	
 	$('#filters input.from').datepicker({
 		startDate: '01-01-2000',
         endDate: new Date()
-	}).on('changeDate', function(e, a){
-		filters.fromDate = e.date;
+	}).on('change', function(e){
+		var date = new Date(e.timeStamp);
+		filters.fromDate = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDay();
 		$(document).trigger(eventTrigger, filters);
 	});
 	
 	$('#filters input.to').datepicker({
 		startDate: '01-01-2000',
         endDate: new Date()
-	}).on('changeDate', function(e, a){
-		filters.toDate = e.date;
+	}).on('change', function(e){
+		var date = new Date(e.timeStamp);
+		filters.toDate = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDay();
 		$(document).trigger(eventTrigger, filters);
 	});
 	
