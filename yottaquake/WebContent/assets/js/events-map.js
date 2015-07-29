219c6b023a998d52a7ca03bd04c506a711811bd7
Christian Vadalà <chr.vadala@gmail.com>
2015-07-29 12:13:55 +0200

    rimuove attribution di terze parti

--- yottaquake/WebContent/assets/js/events-map.js
+++ yottaquake/WebContent/assets/js/events-map.js
@@ -16,9 +16,10 @@ jQuery(document).ready(function ($) {
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
 		minZoom: 2,
 		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+		attribution: 
+			/*'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="http://mapbox.com">Mapbox</a>, ' +
+			'Imagery © <a href="http://mapbox.com">Mapbox</a>, ' +*/
 			'Dev by <strong>Yottabase</strong>',
 		id: 'mapbox.outdoors'
 	}).addTo(map);
