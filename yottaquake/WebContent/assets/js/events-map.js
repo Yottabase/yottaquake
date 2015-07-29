2fc5011f80fd79dd1fd2c756289d34081d9cb6aa
Christian Vadalà <chr.vadala@gmail.com>
2015-07-29 12:12:17 +0200

    set minZoom

--- yottaquake/WebContent/assets/js/events-map.js
+++ yottaquake/WebContent/assets/js/events-map.js
@@ -14,6 +14,7 @@ jQuery(document).ready(function ($) {
 	.setView([mapDefault.lat, mapDefault.lng], mapDefault.zoom);
 	
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
+		minZoom: 2,
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
